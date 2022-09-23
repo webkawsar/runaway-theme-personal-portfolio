6d18b621d7d800346a4f9add94b45d39aaab3bbc
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-09-23 08:16:17 +0600

    added blog section work

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import 'venobox/dist/venobox.min.css';
-import Layout from './components/layout/Layout';
-import ScrollBottom from './components/ScrollBottom';
-import About from './sections/About';
-import Blog from './sections/Blog';
-import Contact from './sections/Contact';
-import ExperienceCountUp from './sections/ExperienceCountUp';
-import Home from './sections/Home';
-import Portfolio from './sections/Portfolio';
-import Services from './sections/Services';
-import SimpleSlider from './sections/SimpleSlider';
-import Skill from './sections/Skill';
-import Testimonial from './sections/Testimonial';
+import "venobox/dist/venobox.min.css";
+import Layout from "./components/layout/Layout";
+import ScrollBottom from "./components/ScrollBottom";
+import About from "./sections/About";
+import Blog from "./sections/Blog";
+import Contact from "./sections/Contact";
+import ExperienceCountUp from "./sections/ExperienceCountUp";
+import Home from "./sections/Home";
+import Portfolio from "./sections/Portfolio";
+import Services from "./sections/Services";
+import Skill from "./sections/Skill";
+import Testimonial from "./sections/Testimonial";
 
 
 const App = () => {
@@ -33,9 +32,6 @@ const App = () => {
           <ScrollBottom />
         </>
       </Layout>
-      <div className='container'>
-        <SimpleSlider />
-      </div>
     </>
   );
 };
--- src/sections/Blog.jsx
+++ src/sections/Blog.jsx
@@ -1,134 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
+import { Fade } from "react-reveal";
 
+const blogsData = [
+  {
+    id: 1,
+    title: `Convallis pulvinar morbi. Aenean nisi vitae metus
+    nonummy a morbi.`,
+    description: `Dictumst integer sollicitudin venenatis ornare quam.
+    Ligula integer luctus, blandit egestas molestie facilisi
+    porttitor neque sodal luctus senectus lacinia euismod
+    adipiscing element turpis dolor curae; posuere augue.`,
+    image: '01.jpg',
+    author: "Rockstar Jack",
+    publishedAt: "20 Jan 2019",
+  },
+  {
+    id: 2,
+    title: `Ornare fames imperdiet sapien. Iaculis dictum aptent
+    commodo at iaculis.`,
+    description: `Dictumst integer sollicitudin venenatis ornare quam.
+    Ligula integer luctus, blandit egestas molestie facilisi
+    porttitor neque sodal luctus senectus lacinia euismod
+    adipiscing element turpis dolor curae; posuere augue.`,
+    image: '02.jpg',
+    author: "Rockstar Jack",
+    publishedAt: "20 Jan 2019",
+  },
+  {
+    id: 3,
+    title: `Vulputate donec sem purus litora varius auctor augue
+    suscipit hac.`,
+    description: `Dictumst integer sollicitudin venenatis ornare quam.
+    Ligula integer luctus, blandit egestas molestie facilisi
+    porttitor neque sodal luctus senectus lacinia euismod
+    adipiscing element turpis dolor curae; posuere augue.`,
+    image: '03.jpg',
+    author: "Rockstar Jack",
+    publishedAt: "20 Jan 2019",
+  },
+];
 const Blog = () => {
+  const [blogs, setBlogs] = useState(blogsData);
+
   return (
-    <section id="blog" name='blog' className="py_80 bg_secondery full_row">
+    <section id="blog" name="blog" className="py_80 bg_secondery full_row">
       <div className="container">
         <div className="row">
-          <div className="col-md-12 col-lg-12">
-            <div className="section_title_1 text-center mx-auto pb_60 wow animated slideInUp">
-              <h2 className="title text-uppercase">
-                <span className="line_double mx-auto color_default">blog</span>
-                What’s News
-              </h2>
-              <span className="sub_title">
-                Interdum a etiam sagittis vehicula porta. Massa felis eros quam
-                blandit nulla dolor habitant. Ullamcorper quis ornare et proin
-                pellentesque.
-              </span>
+          <Fade bottom cascade>
+            <div className="col-md-12 col-lg-12">
+              <div className="section_title_1 text-center mx-auto pb_60 wow animated slideInUp">
+                <h2 className="title text-uppercase">
+                  <span className="line_double mx-auto color_default">
+                    blog
+                  </span>
+                  What’s News
+                </h2>
+                <span className="sub_title">
+                  Interdum a etiam sagittis vehicula porta. Massa felis eros
+                  quam blandit nulla dolor habitant. Ullamcorper quis ornare et
+                  proin pellentesque.
+                </span>
+              </div>
             </div>
-          </div>
+          </Fade>
           <div className="col-md-12 col-lg-12">
             <div className="blog_grid_1 wow animated slideInUp">
               <div className="row">
-                <div className="col-md-12 col-lg-4">
-                  <div className="blog_item">
-                    <div className="comments">
-                      <i className="fa fa-comment" aria-hidden="true"></i>
-                      <span className="color_white">12</span>
-                    </div>
-                    <div className="blog_img overlay_one">
-                      <img src="images/blog/01.jpg" alt="image" />
-                    </div>
-                    <div className="blog_content bg_white color_secondery">
-                      <div className="blog_title">
-                        <a className="color_primary" href="blog-details.html">
-                          <h5>
-                            Convallis pulvinar morbi. Aenean nisi vitae metus
-                            nonummy a morbi.
-                          </h5>
-                        </a>
-                      </div>
-                      <p className="mt_15 mb_30">
-                        Dictumst integer sollicitudin venenatis ornare quam.
-                        Ligula integer luctus, blandit egestas molestie facilisi
-                        porttitor neque sodal luctus senectus lacinia euismod
-                        adipiscing element turpis dolor curae; posuere augue.
-                      </p>
+                {blogs.map((blog) => {
+                  return (
+                    <div key={blog.id} className="col-md-12 col-lg-4">
+                      <div className="blog_item">
+                        <div className="comments">
+                          <i className="fa fa-comment" aria-hidden="true"></i>
+                          <span className="color_white">12</span>
+                        </div>
+                        <div className="blog_img overlay_one">
+                          <img src={`/images/blog/${blog.image}`} alt="image" />
+                        </div>
+                        <div className="blog_content bg_white color_secondery">
+                          <div className="blog_title">
+                            <a
+                              className="color_primary"
+                              href="blog-details.html"
+                            >
+                              <h5>{blog.title}</h5>
+                            </a>
+                          </div>
+                          <p className="mt_15 mb_30">{blog.description}</p>
 
-                      <div className="admin">
-                        <img src="images/about/02.jpg" alt="image" />
-                        <span className="color_white">By - Rockstar Jack</span>
-                      </div>
-                      <div className="date float-right color_primary">
-                        20 Jan 2019
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-12 col-lg-4">
-                  <div className="blog_item">
-                    <div className="comments">
-                      <i className="fa fa-comment" aria-hidden="true"></i>
-                      <span className="color_white">18</span>
-                    </div>
-                    <div className="blog_img overlay_one">
-                      <img src="images/blog/02.jpg" alt="image" />
-                    </div>
-                    <div className="blog_content bg_white color_secondery">
-                      <div className="blog_title">
-                        <a className="color_primary" href="blog-details.html">
-                          <h5>
-                            Ornare fames imperdiet sapien. Iaculis dictum aptent
-                            commodo at iaculis.
-                          </h5>
-                        </a>
-                      </div>
-                      <p className="mt_15 mb_30">
-                        Dictumst integer sollicitudin venenatis ornare quam.
-                        Ligula integer luctus, blandit egestas molestie facilisi
-                        porttitor neque sodal luctus senectus lacinia euismod
-                        adipiscing element turpis dolor curae; posuere augue.
-                      </p>
-
-                      <div className="admin">
-                        <img src="images/about/02.jpg" alt="image" />
-                        <span className="color_white">By - Rockstar Jack</span>
-                      </div>
-                      <div className="date float-right color_primary">
-                        18 Jan 2019
+                          <div className="admin">
+                            <img src="images/about/02.jpg" alt="image" />
+                            <span className="color_white">
+                              By - {blog.author}
+                            </span>
+                          </div>
+                          <div className="date float-right color_primary">
+                            {blog.publishedAt}
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-                <div className="col-md-12 col-lg-4">
-                  <div className="blog_item">
-                    <div className="comments">
-                      <i className="fa fa-comment" aria-hidden="true"></i>
-                      <span className="color_white">23</span>
-                    </div>
-                    <div className="blog_img overlay_one">
-                      <img src="images/blog/03.jpg" alt="image" />
-                    </div>
-                    <div className="blog_content bg_white color_secondery">
-                      <div className="blog_title">
-                        <a className="color_primary" href="blog-details.html">
-                          <h5>
-                            Vulputate donec sem purus litora varius auctor augue
-                            suscipit hac.
-                          </h5>
-                        </a>
-                      </div>
-                      <p className="mt_15 mb_30">
-                        Dictumst integer sollicitudin venenatis ornare quam.
-                        Ligula integer luctus, blandit egestas molestie facilisi
-                        porttitor neque sodal luctus senectus lacinia euismod
-                        adipiscing element turpis dolor curae; posuere augue.
-                      </p>
-
-                      <div className="admin">
-                        <img src="images/about/02.jpg" alt="image" />
-                        <span className="color_white">By - Rockstar Jack</span>
-                      </div>
-                      <div className="date float-right color_primary">
-                        17 Jan 2019
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                  );
+                })}
               </div>
               <div className="mx-auto text-center mt_60">
-                <a className="btn btn-default" href="blog.html">
+                <a className="btn btn-default" href="/blog.html">
                   View Blog
                 </a>
               </div>
