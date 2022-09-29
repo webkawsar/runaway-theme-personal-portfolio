85476943a6c710ce98ac26ded6a87c4f6b2b7424
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-09-29 17:37:42 +0600

    added fade in skill section

--- src/sections/Skill.jsx
+++ src/sections/Skill.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Fade } from "react-reveal";
 
 const skillSets = [
   {
@@ -77,77 +78,85 @@ const Skill = () => {
   return (
     <section id="skill" name="skill" className="py_80 bg_secondery full_row">
       <div className="container">
-        <div className="row">
-          <div className="col-md-12 col-lg-12">
-            <div className="section_title_1 text-center mx-auto pb_60 wow animated slideInUp">
-              <h2 className="title text-uppercase">
-                <span className="line_double mx-auto color_default">skill</span>
-                Design skill
-              </h2>
-              <span className="sub_title">
-                Interdum a etiam sagittis vehicula porta. Massa felis eros quam
-                blandit nulla dolor habitant. Ullamcorper quis ornare et proin
-                pellentesque.
-              </span>
+        <Fade bottom>
+          <div className="row">
+            <div className="col-md-12 col-lg-12">
+              <div className="section_title_1 text-center mx-auto pb_60 wow animated slideInUp">
+                <h2 className="title text-uppercase">
+                  <span className="line_double mx-auto color_default">
+                    skill
+                  </span>
+                  Design skill
+                </h2>
+                <span className="sub_title">
+                  Interdum a etiam sagittis vehicula porta. Massa felis eros
+                  quam blandit nulla dolor habitant. Ullamcorper quis ornare et
+                  proin pellentesque.
+                </span>
+              </div>
             </div>
           </div>
-        </div>
+        </Fade>
         <div className="my_skill">
           <div className="row">
-            <div className="col-md-12 col-lg-6">
-              <div className="about_myskill color_secondery wow animated slideInLeft">
-                <h2 className="color_primary">
-                  Some talk about my professional design skill
-                </h2>
-                <p className="pt_15">
-                  At mattis condimentum leo cubilia dictumst purus cubilia nisl
-                  quisque lacus ultricies proin massa fermentum placerat
-                  sociosqu ornare felis ultricies taciti mauris. Tempor mi, cum
-                  a condimentum commodo bibendum risus mauris natoque molestie
-                  tellus. In iaculis ad augue gravida posuere.
-                </p>
-                <p className="pt_15">
-                  Lectus neque fames lacinia magnis primis. Dictumst torquent
-                  dictumst. Bibendum et rutrum feugiat fames interdum purus
-                  feugiat praesent Nunc vivamus habitant nam ultricies est.
-                  Massa amet cubilia, vitae nonummy nisl. Rutrum mus velit
-                  vivamus sapien est.
-                </p>
+            <Fade left>
+              <div className="col-md-12 col-lg-6">
+                <div className="about_myskill color_secondery wow animated slideInLeft">
+                  <h2 className="color_primary">
+                    Some talk about my professional design skill
+                  </h2>
+                  <p className="pt_15">
+                    At mattis condimentum leo cubilia dictumst purus cubilia
+                    nisl quisque lacus ultricies proin massa fermentum placerat
+                    sociosqu ornare felis ultricies taciti mauris. Tempor mi,
+                    cum a condimentum commodo bibendum risus mauris natoque
+                    molestie tellus. In iaculis ad augue gravida posuere.
+                  </p>
+                  <p className="pt_15">
+                    Lectus neque fames lacinia magnis primis. Dictumst torquent
+                    dictumst. Bibendum et rutrum feugiat fames interdum purus
+                    feugiat praesent Nunc vivamus habitant nam ultricies est.
+                    Massa amet cubilia, vitae nonummy nisl. Rutrum mus velit
+                    vivamus sapien est.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="col-md-12 col-lg-6">
-              <div className="skill-progress wow animated slideInRight">
-                {skills.map((skill) => {
-                  return (
-                    <div key={skill.id} className="prgs-bar fact-counter">
-                      <span>{skill.skillName}</span>
-                      <div
-                        className="progress count wow"
-                        data-wow-duration="0ms"
-                      >
-                        <div className="skill-percent">
-                          <span
-                            className="count-num"
-                            data-speed="3000"
-                            data-stop={skill.percent}
-                          >
-                            {skill.percent}
-                          </span>
-                          %
-                        </div>
+            </Fade>
+            <Fade right>
+              <div className="col-md-12 col-lg-6">
+                <div className="skill-progress wow animated slideInRight">
+                  {skills.map((skill) => {
+                    return (
+                      <div key={skill.id} className="prgs-bar fact-counter">
+                        <span>{skill.skillName}</span>
                         <div
-                          className="progress-bar"
-                          role="progressbar"
-                          aria-valuenow={skill.percent}
-                          aria-valuemax="100"
-                          style={{ width: `${skill.percent}%` }}
-                        ></div>
+                          className="progress count wow"
+                          data-wow-duration="0ms"
+                        >
+                          <div className="skill-percent">
+                            <span
+                              className="count-num"
+                              data-speed="3000"
+                              data-stop={skill.percent}
+                            >
+                              {skill.percent}
+                            </span>
+                            %
+                          </div>
+                          <div
+                            className="progress-bar"
+                            role="progressbar"
+                            aria-valuenow={skill.percent}
+                            aria-valuemax="100"
+                            style={{ width: `${skill.percent}%` }}
+                          ></div>
+                        </div>
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
               </div>
-            </div>
+            </Fade>
           </div>
         </div>
       </div>
