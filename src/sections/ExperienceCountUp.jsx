c33afbb54ca8ee13c8af38bbffaad91bac48d8eb
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-10-04 10:22:34 +0600

    countup section icon make dynamic

--- src/sections/ExperienceCountUp.jsx
+++ src/sections/ExperienceCountUp.jsx
@@ -1,32 +1,36 @@
 import React, { useContext, useEffect, useState } from "react";
 import CountUp from "react-countup";
+import * as FaIcons from "react-icons/fa";
 import { useInView } from "react-intersection-observer";
 import { BlogContext } from "../context/Blog.context";
-
 
 const initialData = [
   {
     title: "Years of Experience",
     value: 0,
-    duration: 0
+    duration: 0,
+    id: 1,
   },
   {
     title: "Projects Done",
     value: 0,
-    duration: 0
+    duration: 0,
+    id: 2,
   },
   {
     title: "Happy Clients",
     value: 0,
-    duration: 0
-  }
+    duration: 0,
+    id: 3
+  },
 ];
 
 const ExperienceCountUp = () => {
-  const {myInfo} = useContext(BlogContext);
+  const { myInfo } = useContext(BlogContext);
   const [counts, setCounts] = useState(initialData);
-  const {factCounts} = myInfo.countupSection;
+  const { factCounts } = myInfo.countupSection;
   const [ref, inView] = useInView();
+
 
   useEffect(() => {
     if (inView) {
@@ -44,33 +48,35 @@ const ExperienceCountUp = () => {
           <div className="col-md-12 col-lg-12">
             <div className="fact-counter">
               <div className="row">
-
                 {counts.map((counter) => {
+                  let icon = counter.icon ? counter.icon : 'FaCertificate';
+                  const DynamicIcon = FaIcons[icon];
+
                   return (
-                    <div key={counter.title} className="col-md-4 col-lg-4">
+                    <div key={counter.id} className="col-md-4 col-lg-4">
                       <div
                         className="counter count wow text-center"
                         data-wow-duration="300ms"
                       >
-                        <span className="flaticon-man-working-on-a-laptop-from-side-view"></span>
+                        <DynamicIcon size={40} color="white" />
                         <div className="counting_digit color_default mt_15">
                           <h2
                             className="count-num"
                             data-speed="3000"
                             data-stop="7"
                           >
-                            <CountUp end={counter.value} duration={counter.duration} />
+                            <CountUp
+                              end={counter.value}
+                              duration={counter.duration}
+                            />
                           </h2>
                           <span>+</span>
                         </div>
-                        <h3 className="color_white mt_15">
-                          {counter.title}
-                        </h3>
+                        <h3 className="color_white mt_15">{counter.title}</h3>
                       </div>
                     </div>
                   );
                 })}
-
               </div>
             </div>
           </div>
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-// import { FaDesktop } from "react-icons/fa";
 import * as FaIcons from "react-icons/fa";
 import { Fade } from "react-reveal";
 import { BlogContext } from "../context/Blog.context";
@@ -7,9 +6,7 @@ import { BlogContext } from "../context/Blog.context";
 const Services = () => {
   const { myInfo } = useContext(BlogContext);
   const { serviceIntro, services } = myInfo.serviceSection;
-  console.log(myInfo, "myInfo");
- 
-  
+
 
   return (
     <section id="services" name="services" className="py_80 full_row bg_white">
@@ -24,11 +21,7 @@ const Services = () => {
                   </span>
                   What i do
                 </h2>
-                <span className="sub_title">
-                  Interdum a etiam sagittis vehicula porta. Massa felis eros
-                  quam blandit nulla dolor habitant. Ullamcorper quis ornare et
-                  proin pellentesque.
-                </span>
+                <span className="sub_title">{serviceIntro}</span>
               </div>
             </div>
           </Fade>
@@ -37,15 +30,19 @@ const Services = () => {
               <div className="row">
                 <Fade bottom cascade>
                   {services.map((service) => {
-                    
                     let icon = service.icon;
                     const DynamicIcon = FaIcons[icon];
-                    
+
                     return (
-                      <div className="col-md-6 col-lg-4">
+                      <div key={service.idNo} className="col-md-6 col-lg-4">
                         <div className="service_two text-center pt_15 mb_30 wow animated slideInUp">
                           <div className="srv_item_number color_lightgray">
-                            <strong>{service.idNo < 10 ? `0${service.idNo}` : service.idNo}.</strong>
+                            <strong>
+                              {service.idNo < 10
+                                ? `0${service.idNo}`
+                                : service.idNo}
+                              .
+                            </strong>
                           </div>
                           <h3 className="p_20 text-uppercase color_primary">
                             {service.name}
@@ -53,106 +50,11 @@ const Services = () => {
                           <div className="srv_icon color_white d-flex align-items-center justify-content-center">
                             <DynamicIcon size={40} />
                           </div>
-                          <p>
-                            {service.intro}
-                          </p>
+                          <p>{service.intro}</p>
                         </div>
                       </div>
                     );
                   })}
-
-                  <div className="col-md-6 col-lg-4">
-                    <div className="service_two text-center pt_15 mb_30 wow animated slideInUp">
-                      <div className="srv_item_number color_lightgray">
-                        <strong>02.</strong>
-                      </div>
-                      <h3 className="p_20 text-uppercase color_primary">
-                        Web Development
-                      </h3>
-                      <div className="srv_icon color_white">
-                        <span className="flaticon-programming"></span>
-                      </div>
-                      <p>
-                        Proin a ullamcorper et primis lobortis laoreet senectus.
-                        Vitae dignissim sollicitudin eleifend cursus tempus
-                        curabitur posuere nam arcu platea sodales.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="col-md-6 col-lg-4">
-                    <div className="service_two text-center pt_15 mb_30 wow animated slideInUp">
-                      <div className="srv_item_number color_lightgray">
-                        <strong>03.</strong>
-                      </div>
-                      <h3 className="p_20 text-uppercase color_primary">
-                        Wordpress
-                      </h3>
-                      <div className="srv_icon color_white">
-                        <span className="flaticon-wordpress-logo"></span>
-                      </div>
-                      <p>
-                        Proin a ullamcorper et primis lobortis laoreet senectus.
-                        Vitae dignissim sollicitudin eleifend cursus tempus
-                        curabitur posuere nam arcu platea sodales.
-                      </p>
-                    </div>
-                  </div>
-                </Fade>
-                <Fade bottom cascade>
-                  <div className="col-md-6 col-lg-4">
-                    <div className="service_two text-center pt_15 mb_30 wow animated slideInUp">
-                      <div className="srv_item_number color_lightgray">
-                        <strong>04.</strong>
-                      </div>
-                      <h3 className="p_20 text-uppercase color_primary">
-                        Graghic Design
-                      </h3>
-                      <div className="srv_icon color_white">
-                        <span className="flaticon-editor"></span>
-                      </div>
-                      <p>
-                        Proin a ullamcorper et primis lobortis laoreet senectus.
-                        Vitae dignissim sollicitudin eleifend cursus tempus
-                        curabitur posuere nam arcu platea sodales.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="col-md-6 col-lg-4">
-                    <div className="service_two text-center pt_15 mb_30 wow animated slideInUp">
-                      <div className="srv_item_number color_lightgray">
-                        <strong>05.</strong>
-                      </div>
-                      <h3 className="p_20 text-uppercase color_primary">
-                        Branding
-                      </h3>
-                      <div className="srv_icon color_white">
-                        <span className="flaticon-internet"></span>
-                      </div>
-                      <p>
-                        Proin a ullamcorper et primis lobortis laoreet senectus.
-                        Vitae dignissim sollicitudin eleifend cursus tempus
-                        curabitur posuere nam arcu platea sodales.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="col-md-6 col-lg-4">
-                    <div className="service_two text-center pt_15 mb_30 wow animated slideInUp">
-                      <div className="srv_item_number color_lightgray">
-                        <strong>06.</strong>
-                      </div>
-                      <h3 className="p_20 text-uppercase color_primary">
-                        Opencart
-                      </h3>
-                      <div className="srv_icon color_white">
-                        <span className="flaticon-shopping-cart"></span>
-                      </div>
-                      <p>
-                        Proin a ullamcorper et primis lobortis laoreet senectus.
-                        Vitae dignissim sollicitudin eleifend cursus tempus
-                        curabitur posuere nam arcu platea sodales.
-                      </p>
-                    </div>
-                  </div>
                 </Fade>
               </div>
             </div>
